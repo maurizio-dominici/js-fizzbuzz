3c6a1bba15c6d75367885357845ed6828ea2597e
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-02 15:06:39 +0200

    add for

---
+++ fizzbuzz/js/main.js
@@ -0,0 +1,14 @@
+// SCRIVO UN CICLO FOR PER CONTARE I NUMERI DA 1 A 100
+
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+  //   const multiplex3 = i % 3 === 0;
+  //   const multiplex5 = i % 5 === 0;
+  //   if (multiplex3) {
+  //     console.log("Fizz");
+  //   }
+
+  //   if (multiplex5) {
+  //     console.log("Buzz");
+  //   }
+}
